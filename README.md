ebd663102d6addd92212dff6d390951acb089abc
Arash Molavi <ironyh@users.noreply.github.com>
2023-04-27 16:12:21 +0000

    Simplify

--- README.md
+++
@@ -1,5 +0,0 @@
-# dotfiles
-Dotfiles
-
-Installing
-Change the
--- install.sh
+++ install.sh
@@ -1,40 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+# Install oh-my-zsh
 
-
-create_symlinks() {
-    # Get the directory in which this script lives.
-    script_dir=$(dirname "$(readlink -f "$0")")
-
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
-
-    # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
+zshrc() {
+                 
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+    
+    
+    cat .zshrc > $HOME/.zshrc
 }
 
-create_symlinks
-
-echo "Initializing conda for zsh."
-conda init zsh
-
-echo "Installing fonts."
-FONT_DIR="$HOME/.fonts"
-git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
-cd $FONT_DIR
-
-
-echo "Installing laravel sail plugin"
-git clone https://github.com/ariaieboy/laravel-sail ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/laravel-sail
-
-
-echo "Setting up the Spaceship theme."
-ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-
-
-./install.sh
+zshrc
