71f8f1fb9c31fd9fb6c09b1a347bdd1b1176acbf
Arash Molavi <ironyh@users.noreply.github.com>
2023-05-01 17:34:27 +0000

    Commiting alot

--- .zshrc
+++ .zshrc
@@ -1,14 +1,9 @@
-# Path to your oh-my-zsh installation.
 export ZSH="${HOME}/.oh-my-zsh"
-
-# Theme.
-ZSH_THEME="spaceship"
-export SPACESHIP_DIR_TRUNC=0
+...
 
 # Plugins.
 plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
-# Set colors for LS_COLORS.
-eval `dircolors ~/.dircolors`
+ZSH_THEME=bira
--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 create_symlinks() {
     # Get the directory in which this script lives.
     script_dir=$(dirname "$(readlink -f "$0")")
@@ -19,14 +18,5 @@ create_symlinks() {
 
 create_symlinks
 
-echo "Installing fonts."
-FONT_DIR="$HOME/.fonts"
-git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
-cd $FONT_DIR
 
-./install.sh
-
-echo "Setting up the Spaceship theme."
-ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+apt-get install zsh
