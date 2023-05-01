877a730833c5dbfb6e32d5e87b512db770de1618
Arash Molavi <ironyh@users.noreply.github.com>
2023-05-01 14:07:29 +0000

    Removed customization

--- install.sh
+++ install.sh
@@ -19,13 +19,11 @@ create_symlinks() {
 
 create_symlinks
 
-echo "Initializing conda for zsh."
-conda init zsh
-
 echo "Installing fonts."
 FONT_DIR="$HOME/.fonts"
 git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
 cd $FONT_DIR
+
 ./install.sh
 
 echo "Setting up the Spaceship theme."
